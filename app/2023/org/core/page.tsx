91d77fb2e249e1078f40ffd270bba5a89ee03bb1
Achyut2009 <achyutkpaliwal@gmail.com>
2025-04-26 23:15:35 +0300

    changes done

--- app/2023/org/core/page.tsx
+++ app/2023/org/core/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import {  twentythree } from "@/data";
+import { twentythree } from "@/data";
 import Image from "next/image";
 import {
   Dialog,
--- app/2024/org/core/page.tsx
+++ app/2024/org/core/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import {  twentyfour, twentythree } from "@/data";
+import { twentyfour } from "@/data";
 import Image from "next/image";
 import {
   Dialog,
