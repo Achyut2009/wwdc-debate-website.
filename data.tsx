56f50cfdce0d4d8d6edd324adbe93fb8acb25306
AchyutPaliwal <achyutkpaliwal@gmail.com>
2025-05-20 22:55:22 +0300

    179th commit

--- data.tsx
+++ data.tsx
@@ -480,6 +480,9 @@ const twentytwo = {
       body:[
         <span className="font-bold">Infoslide: </span>, 
         "Legacy preference or legacy admission is a preference given by an institution or organization to certain applicants on the basis of their familial relationship to alumni of that institution. For example, students getting admitted into ivy league colleges since their parents are alumni.",
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "Motion: This House Regrets Legacy Admission.",
       ],
@@ -504,7 +507,9 @@ const twentytwo = {
       body:[
         <span className="font-bold">Infoslide: </span>, 
         "Infoslide: Teenage Dream refers to the concept of living your life the best way as a teenager. (Example, hanging out with friends, having fun, partying, etc. all while having a strong academic standing)",
-        " ",
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "Motion: This House Regrets the glorification of the Teenage Dream.",
       ],
@@ -514,6 +519,9 @@ const twentytwo = {
       body:[
         <span className="font-bold">Infoslide: </span>, 
         "For the purpose of this debate, radical environmentalism refers to the use of violent practices to further the cause of environmental movements due to ecocentrism-based frustration. (Example: throwing pea soup at Van Gogh’s “The Sower”, spraying orange paints on buildings in central London, etc.) ",
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>,
         "Motion: This House Supports radical environmentalism.",
       ],
@@ -639,11 +647,17 @@ const twentythree = {
     {
       title: "Round 1 & Quarter Finals (Prepared)",
       body:[
-        <span className="text-bold">Theme:</span>,
+        <span className="font-bold">Theme:</span>,
         "International Relations", 
+        <span>
+
+        </span>,
         <span className="font-bold">Infoslide: </span>, 
         "In July of 2021, the Biden Administration imposed new sanctions on Cuba following the Cuban government’s crackdown on demonstrators in the streets of Cuba protesting the shortages in medicine and food resulting from the COVID-19 pandemic.",
         "Biden’s policy was being framed by Senator Robert Mendez, Chairman of the US Senate Foreign Relations Committee, and the son of Cuban immigrants. Mendez has, in the past, been critical of the Obama Administrations softer line on Cuba.", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "This House Believes That the United States Should Maintain Its Embargo against Cuba.",
       ]
@@ -653,6 +667,9 @@ const twentythree = {
       body:[
         <span className="font-bold">Theme: </span>,
         "Criminal Justice", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Context: </span>, 
         '"The right to survival prevails over property,” the court of appeals ruled when reviewing the case of a homeless man who had been given a six month jail sentence and a €100 euro fine for stealing cheese and sausages."',
       ]
@@ -672,8 +689,14 @@ const twentythree = {
       body:[
         <span className="font-bold">Theme: </span>,
         "Social Issues",
+        <span>
+          
+        </span>,
         <span className="font-bold">Infoslide: </span>, 
         "Effective Altruism is the view that we should invest energy into figuring out what the most effective way to help others is, and encourages people to act on that basis. Effective Altruists often encourage donations to highly efficient charities, such as the Malaria Consortium. Long-termism is the view that we should be doing much more to help future generations. Effective Altruists who are long-termists often advocate for greater preparedness against existential risks, such as hostile AI takeover, or pandemics.", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "This House Believes That the Effective Altruism community should prioritize alleviating short-term suffering over long-termism",
       ]
@@ -683,6 +706,9 @@ const twentythree = {
       body:[
         <span className="font-bold">Infoslide: </span>, 
         "Teenage Dream refers to the concept of living your life the best way as a teenager. (Example, hanging out with friends, having fun, partying, etc. all while having a strong academic standing)", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "This House Regrets the glorification of the Teenage Dream.",
       ]
@@ -692,6 +718,9 @@ const twentythree = {
       body:[
         <span className="font-bold">Infoslide: </span>, 
         "Progressive rebranding refers to actions undertaken by media companies to rewrite, reword, or remake works of art that are allegedly offensive or out-of-touch with the current social norm, to make them more inclusive and sensitive. Such changes are new to the work of art, and not intended nor included by the original creator of the work. Recent examples include: The rewriting of Augustus Gloop in Roald Dahl’s Charlie and the Chocolate Factory to be “enormous” instead of “fat”, and the remaking of Aladdin by Disney to give more independent screen time to the character of Jasmine, who was previously given almost no independent screen time", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "This House supports the trend of media and publishing companies (e.g. Disney, Penguin Books, Warner Brothers, Bloomsbury) engaging in progressive rebranding",
       ]
@@ -706,8 +735,14 @@ const twentythree = {
       body:[
         <span className="text-bold">Theme: </span>,
         "Artificial Intelligence", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Infoslide: </span>, 
         "TalkToys are dolls with Artificial Intelligence (AI) and speech recognition software that enable them to engage in an actual conversation with children (they have a lexicon of thousands of prescript responses). TalkToys are meant to serve as a companion to the child, and they are in the late stages of development. Prominent examples of toys that will be able to talk include Barbie, G.I Joe and Star Wars and Action Figures.", 
+        <span>
+          
+        </span>,
         <span className="font-bold">Motion: </span>, 
         "This House would ban Talk Toys",
       ]
