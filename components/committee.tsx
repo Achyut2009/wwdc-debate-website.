f473366d51d443704b29a0a520f8e7efaad443b9
Achyut Paliwal <160041506+Achyut2009@users.noreply.github.com>
2025-06-14 14:25:50 +0300

    Update committee.tsx

--- components/committee.tsx
+++ components/committee.tsx
@@ -263,7 +263,7 @@ export default async function Committee() {
         <div className="container mx-auto px-4">
           {/* Org Teaching Team */}
           <div className="flex justify-center">
-            <div className="grid grid-flow-col grid-rows-2 gap-12 justify-center">
+            <div className="grid grid-flow-col grid-rows-2 grid-row-reverse gap-12 justify-center">
               {orgteach.map((member, index) => (
                 <div key={`orgteach-${index}`} className="flex flex-col items-center">
                   {/* Mobile view */}
@@ -358,7 +358,7 @@ export default async function Committee() {
           
           {/* Org Committee Members */}
           <div className="flex justify-center mt-16">
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:gap-12 w-full max-w-5xl justify-self-center place-items-center">
+            <div className="grid grid-cols-2 md:grid-flow-col md:grid-rows-2 md:grid-row-reverse gap-10 md:gap-12 w-full max-w-5xl justify-self-center place-items-center">
               {org.map((member, index) => (
                 <div key={`org-${index}`} className="flex flex-col items-center">
                   {/* Mobile view */}
